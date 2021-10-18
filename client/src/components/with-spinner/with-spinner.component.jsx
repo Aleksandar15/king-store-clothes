fdb139b35520d725acef2a67c03c5eae2dd5d3c6
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2021-10-18 04:32:01 +0200

    Fixed with-spinner.component.jsx

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import Spinner from "../spinner/spinner.component";
 
 
-const WithSpinner = WrappedComponent => Spinner= ({ isLoading, ...otherProps })=>{
+const WithSpinner = WrappedComponent =>  ({ isLoading, ...otherProps })=>{
     return isLoading ? (
       <Spinner/>
     ) : (
